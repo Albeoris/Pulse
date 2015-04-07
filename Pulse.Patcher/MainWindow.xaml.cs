868ad496a810502eca8db32008e216d3fbfbd090
Albeoris <albeoris@gmail.com>
2015-04-07 21:57:00 +0300

    Optional DmoMp3FirameDecompressor

--- Pulse.Patcher/MainWindow.xaml.cs
+++ Pulse.Patcher/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using NAudio.FileFormats.Mp3;
 using NAudio.Wave;
 using Pulse.Core;
 using Pulse.UI;
@@ -34,13 +35,25 @@ namespace Pulse.Patcher
             {
                 Stream stream = _disposables.Add(Assembly.GetExecutingAssembly().GetManifestResourceStream("Pulse.Patcher.Background.mp3"));
                 IWavePlayer waveOutDevice = _disposables.Add(new WaveOut());
-                Mp3FileReader audioFileReader = _disposables.Add(new Mp3FileReader(stream));
+                Mp3FileReader audioFileReader = _disposables.Add(new Mp3FileReader(stream, CreateMp3Decompressor));
                 waveOutDevice.Init(audioFileReader);
                 waveOutDevice.Play();
             }
             catch (Exception ex)
             {
                 UiHelper.ShowError(this, ex);
+            }
+        }
+
+        private IMp3FrameDecompressor CreateMp3Decompressor(WaveFormat mp3Format)
+        {
+            try
+            {
+                return new AcmMp3FrameDecompressor(mp3Format);
+            }
+            catch
+            {
+                return new DmoMp3FrameDecompressor(mp3Format);
             }
         }
 
